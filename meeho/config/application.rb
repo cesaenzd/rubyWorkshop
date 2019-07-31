d73f981743166f9c5d9c3125de36727644d7b215
cesar <cdsaenzsalazar@gmail.com>
2019-07-30 20:09:17 -0600

    6th Class

--- meeho/config/application.rb
+++ meeho/config/application.rb
@@ -11,6 +11,17 @@ module Meeho
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:            'smpt.gmail.com',
+      port:               587,
+      domain:             'gmail.com',
+      user_name:          'no.reply.meeho',
+      password:           'RubySchool2019!',
+      authentication:     'plain',
+      anable_starttls_auto: true
+    }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
